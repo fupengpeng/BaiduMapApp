fa74cabc86ef17cac9026b7a4f2f520ab1cd78b8
564055710@qq.com <564055710@qq.com>
2018-04-08 09:46:50 +0800

    Initial commit

---
+++ app/src/main/java/club/zhisimina/baidumapapp/search/ShareDemo.java
@@ -0,0 +1,25 @@
+package club.zhisimina.baidumapapp.search;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import club.zhisimina.baidumapapp.R;
+
+public class ShareDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_demo);
+    }
+
+    public void startShareDemo(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, ShareDemoActivity.class);
+        startActivity(intent);
+
+    }
+
+}
